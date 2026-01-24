c4e894bdeeb55994ce35ae1f2c22a38b4cd48470
Misha <mikhailbalyberdin@gmail.com>
2026-01-24 10:51:17 -0500

    make basic form structure

--- src/view/form/formParams.js
+++ src/view/form/formParams.js
@@ -2,9 +2,51 @@ export const formParams = {
   class: [],
   tagName: "form",
 };
+export const inputContainerParams = {
+  class: [],
+  tagName: "div",
+};
 
-export const inputParams = {
+export const inputTextParams = {
   class: [],
   tagName: "input",
   attr: { type: "text", id: "note" },
+};
+
+export const labelParams = {
+  class: [],
+  tagName: "label",
+};
+
+export const fakeCheckboxParams = {
+  class: [],
+  tagName: "span",
+};
+
+export const realCheckboxParams = {
+  class: [],
+  tagName: "input",
+  attr: { type: "checkbox", id: "star" },
+};
+
+export const textAreaParams = {
+  class: [],
+  tagName: "textarea",
+};
+
+export const buttonContainerParams = {
+  class: [],
+  tagName: "div",
+};
+
+export const submitButtonParams = {
+  class: [],
+  tagName: "button",
+  attr: { type: "submit" },
+};
+
+export const cancelButtonParams = {
+  class: [],
+  tagName: "button",
+  attr: { type: "reset" },
 };
--- src/view/form/form_view.js
+++ src/view/form/form_view.js
@@ -1,14 +1,38 @@
 import Creator from "../creator";
-import { formParams, inputParams } from "./formParams";
+import {
+  inputContainerParams,
+  formParams,
+  realCheckboxParams,
+  inputTextParams,
+  labelParams,
+  fakeCheckboxParams,
+  textAreaParams,
+  buttonContainerParams,
+  submitButtonParams,
+  cancelButtonParams,
+} from "./formParams";
 export class FormView {
   constructor() {
-    this.form = new Creator(formParams).getElement();
-    this.input = new Creator(inputParams).getElement();
+    this.form = null;
     this.build();
   }
 
   build() {
-    this.form.append(this.input);
+    this.form = new Creator(formParams).getElement();
+    this.inputContainer = new Creator(inputContainerParams).getElement();
+    this.textInput = new Creator(inputTextParams).getElement();
+    this.label = new Creator(labelParams).getElement();
+    this.checkboxReal = new Creator(realCheckboxParams).getElement();
+    this.checkboxFake = new Creator(fakeCheckboxParams).getElement();
+    this.textArea = new Creator(textAreaParams).getElement();
+    this.buttonContainer = new Creator(buttonContainerParams).getElement();
+    this.submitButton = new Creator(submitButtonParams).getElement();
+    this.cancelButton = new Creator(cancelButtonParams).getElement();
+
+    this.label.append(this.checkboxFake, this.checkboxReal);
+    this.inputContainer.append(this.textInput, this.label);
+    this.buttonContainer.append(this.submitButton, this.cancelButton);
+    this.form.append(this.inputContainer, this.textArea, this.buttonContainer);
   }
 
   getForm() {
